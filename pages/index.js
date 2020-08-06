35298e2d1ff87e79b8c22b2f3ab0a155664ac7f5
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-08-06 09:24:54 +0200

    Adds frontpage

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+const HomePage = () => {
+  return (
+    <>
+      <div className='flex flex-col items-center'>
+        <h1 className='text-4xl font-mono'>Yatzy</h1>
+        <div>Plays the game... so you don't have to</div>
+      </div>
+    </>
+  )
+}
+
+export default HomePage
