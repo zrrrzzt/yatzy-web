4ae66031ffeedbed0ca21bb01cea71b92414abd2
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-08-07 08:41:51 +0200

    Fixes scoreCard

--- components/score-card.js
+++ components/score-card.js
@@ -1,8 +1,30 @@
+const Header = props => {
+  return (
+    <thead>
+      <tr>
+        { Object.keys(props).map(item => <th className='px-4 py-2'>{item}</th>)}
+      </tr>
+    </thead>
+  )
+}
+
+const Row = props => {
+  return (
+    <thead>
+      <tr>
+        { Object.values(props).map(item => <td className='border px-4 py-2'>{item}</td>)}
+      </tr>
+    </thead>
+  )
+}
+
 const scoreCard = props => {
   const { results } = props
   return (
     <div>
-      { JSON.stringify(results) }
+      <table class="table-auto mb-4">
+        { results.map((row, index) => index === 0 ? <><Header {...row} /><Row {...row} /></> : <Row {...row} />) }
+      </table>
     </div>
   )
 }
