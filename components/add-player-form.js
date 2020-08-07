6115699485a67424daa68f02e3b750e1bea7ee72
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-08-07 08:52:13 +0200

    Makes form behave as form

--- components/add-player-form.js
+++ components/add-player-form.js
@@ -1,20 +1,21 @@
 const AddPlayerForm = props => {
   const { addPlayer } = props
-  const handleClick = () => {
+  const handleSubmit = event => {
+    event.preventDefault()
     const nameField = document.getElementById('playername')
     addPlayer(nameField.value)
     nameField.value = ''
   }
 
   return (
-    <div className='w-full max-w-sm'>
+    <form className='w-full max-w-sm' onSubmit={handleSubmit}>
       <div className='flex items-center border-b border-teal-500 py-2'>
         <input className='appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none' type='text' placeholder='Player name' aria-label='Player name' id='playername' />
-        <button onClick={handleClick} className='flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded' type='button'>
+        <button className='flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded' type='submit'>
           Add
         </button>
       </div>
-    </div>
+    </form>
   )
 }
 
