ae41c4db4a4b0efe4aa437167af320d44563ca5d
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-08-08 17:34:49 +0200

    Implements dice faces (patch)

--- components/dice.js
+++ components/dice.js
@@ -3,7 +3,7 @@ import Die from './die'
 const Dice = props => {
   const { dice } = props
   return (
-    <div>
+    <div className='flex flex-row'>
       {dice.map((value, index) => <Die value={value} key={index} />)}
     </div>
   )
