e0e672432c42e823a5f0f7bf160564fc6b8085b0
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-08-09 08:53:52 +0200

    Styles scorecard (patch)

--- components/score-card.js
+++ components/score-card.js
@@ -9,7 +9,7 @@ const Row = props => {
 const Header = props => {
   return (
     <tr>
-      {Object.keys(props).map((item, index) => <th className='px-4 py-2' key={index}>{item}</th>)}
+      {Object.keys(props).map((item, index) => <th className='px-4 py-2 text-left' key={index}>{item}</th>)}
     </tr>
   )
 }
@@ -18,7 +18,7 @@ const scoreCard = props => {
   const { results } = props
   return (
     <div>
-      <table className='table-auto mt-2 mb-4'>
+      <table className='table-auto mt-2 mb-4 rounded shadow-lg'>
         <thead>
           <Header {...results[0]} />
         </thead>
