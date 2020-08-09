6131ca1a6d16303ebb1e60d5523dae92a40111ed
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-08-09 09:08:22 +0200

    Styles game table (patch)

--- components/game-table.js
+++ components/game-table.js
@@ -4,9 +4,8 @@ const GameTable = props => {
   const { name, rolls, round } = props
 
   return (
-    <div>
-      <h2 className='text-2xl'>Round: {round}</h2>
-      <h2 className='text-2xl'>Player: {name}</h2>
+    <div className='rounded shadow-lg mt-2 mb-4 p-8'>
+      <h2 className='text-2xl mb-2'>{round}: {name}</h2>
       <div>
         {rolls && <Dice rolls={rolls} />}
       </div>
