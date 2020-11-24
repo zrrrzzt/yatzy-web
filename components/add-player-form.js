794349dd395b8ebb36ff361d104959bad4c5d7cc
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-11-24 19:08:42 +0100

    Updates text color (patch)

--- components/add-player-form.js
+++ components/add-player-form.js
@@ -11,7 +11,7 @@ const AddPlayerForm = props => {
     <form className='w-full max-w-sm' onSubmit={handleSubmit}>
       <div className='flex items-center border-b border-teal-500 py-2'>
         <input className='appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none' type='text' placeholder='Player name' aria-label='Player name' id='playername' />
-        <button className='flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded' type='submit'>
+        <button className='flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-black py-1 px-2 rounded' type='submit'>
           Add
         </button>
       </div>
